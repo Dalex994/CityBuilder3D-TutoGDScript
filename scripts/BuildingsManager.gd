f338f68f3eb43aec64bdf8c56f194feb3aec37bd
Dalex994 <dalex.laville@protonmail.com>
2024-04-25 21:33:09 +0200

    implementing buildings selection via UI

--- scripts/BuildingsManager.gd
+++ scripts/BuildingsManager.gd
@@ -1,21 +1,51 @@
 extends Node
 
 # Buildings List
-var building_1 = "assets/buildings/building.tscn"
-var building_2 = "assets/buildings/building_info.tscn"
+var building_info = "assets/buildings/building_info.tscn"
+var building_clean = "assets/buildings/building_clean.tscn"
+var building_repair = "assets/buildings/building_repair.tscn"
+var building_toilet = "assets/buildings/building_toilet.tscn"
+var building_wheel = "assets/buildings/wheel.tscn"
+var building_tower = "assets/buildings/tower.tscn"
+var building_swing = "assets/buildings/swing_3.tscn"
+var building_circus = "assets/buildings/circus.tscn"
+var building_sitswing = "assets/buildings/sit_swing.tscn"
 
-var grid_data = [] # 40*40 = 1600 fictives cases of the map grid
+# 40*40 = 1600 fictives cases of the map grid
+var grid_data = [] 
+
+var selected_building = 2
 
 func _ready():
 	for i in range(1600):
 		grid_data.append(0) # 0 = empty grid case
+		
+func get_selected_building():
+	if selected_building == 1:
+		return building_info
+	if selected_building == 2:
+		return building_clean
+	if selected_building == 3:
+		return building_repair
+	if selected_building == 4:			#TODO: upgrade it with a switch ^^
+		return building_toilet
+	if selected_building == 5:
+		return building_wheel
+	if selected_building == 6:
+		return building_tower
+	if selected_building == 7:
+		return building_swing
+	if selected_building == 8:
+		return building_circus
+	if selected_building == 9:
+		return building_sitswing
 
 func create_building(position, grid_position):
 	
 	if grid_data[grid_position] != 1:
 		# Building selected
 		print("Nothing on... Built")	#DEBUG
-		var building_res = load(building_2)
+		var building_res = load(get_selected_building())
 		var building = building_res.instantiate()
 		get_tree().get_root().add_child(building)
 		building.global_position = position
---
+++ scripts/GUI.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+# building selection
+func _on_button_pressed():
+	BuildingsManager.selected_building = 1
+	
+func _on_button_2_pressed():
+	BuildingsManager.selected_building = 2
+
+func _on_button_3_pressed():
+	BuildingsManager.selected_building = 3
+	
+func _on_button_4_pressed():
+	BuildingsManager.selected_building = 4
+	
+func _on_button_5_pressed():
+	BuildingsManager.selected_building = 5
+	
+func _on_button_6_pressed():
+	BuildingsManager.selected_building = 6
+	
+func _on_button_7_pressed():
+	BuildingsManager.selected_building = 7
+	
+func _on_button_8_pressed():
+	BuildingsManager.selected_building = 8
+
+func _on_button_9_pressed():
+	BuildingsManager.selected_building = 9
